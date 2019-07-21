561f72f26d120271eac69dd6cb6433eb644188be
Noah Giebink <nwgiebink@gmail.com>
2019-07-21 12:55:22 -0700

    Start script for data manipulation and exploration

---
+++ scripts/diadasia_nestObs.R
@@ -0,0 +1,14 @@
+#Diadasia Nest Observations
+#Describe nest activity in D. rinconis aggregation
+#Noah Giebink
+#giebink@email.arizona.edu
+#2019-07-21
+
+#Packages
+library(tidyverse)
+
+#Data
+
+nestObs_raw <- read.csv("data/diadasia_nest_observations.csv")
+
+#Data tidying: make long
