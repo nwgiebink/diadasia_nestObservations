6a2b106db5556d325f9bb73193a85dc6cdb06a9e
Noah Giebink <nwgiebink@gmail.com>
2019-07-23 14:22:58 -0700

    tidy data etc.

--- scripts/diadasia_nestObs.R
+++ scripts/diadasia_nestObs.R
@@ -9,6 +9,26 @@ library(tidyverse)
 
 #Data
 
-nestObs_raw <- read.csv("data/diadasia_nest_observations.csv")
+nestObs_raw <- read.csv("data/diadasia_nest_observations2.csv")
 
-#Data tidying: make long
+#clean data and add variable for gaurded 
+nestObs_clean <- select(nestObs_raw, -notes) %>%
+  mutate(nest_type = ifelse(guard > 0, "guarded", "unguarded"))
+
+
+
+#Data tidying: make long, add behavior_duration for guarding
+nestObs_clean_tidy <- gather(nestObs_clean, behavior, count,
+       -nest, -observation_seconds, -start_time, -day,
+       -month, -year, -nest_type) %>%
+  uncount(count) %>%
+  arrange((nest), desc(nest)) %>%
+  mutate(behavior_duration = 
+           ifelse(behavior == "guard", "", 0)) %>%
+  select(nest, nest_type, behavior, behavior_duration, 
+         year, month, day, start_time, observation_seconds)
+
+#write nestObs_clean_tidy as .csv
+write_csv(nestObs_clean_tidy, path = "./data/diadasia_nestObs_tidy.csv", col_names = TRUE)
+
+
